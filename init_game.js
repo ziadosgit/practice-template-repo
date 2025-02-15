e5c66df00035c01ee2e3e1fad83c719786a7e208
ziadosgit <ZiadOsamaS2008@gmail.com>
2025-02-15 10:11:16 +0200

    Create init_game.js

---
+++ init_game.js
@@ -0,0 +1,13 @@
+const ps = require("prompt-sync");
+const prompt = ps();
+function initializeGame() {
+    return new Promise(function (resolve, reject) {
+        let character = prompt("Enter your name: ");
+        setTimeout(() => {
+            resolve(character);
+        }, 10000);
+    }).then(data => {
+        console.log(`Hi ${data}, the game initialized successfully`);
+    });
+}
+initializeGame();
