dfdccb339ad1cf21eb2dcbccb77c713aabf5d2ee
hamza <hamzaalwagih123@gmail.com>
2025-02-15 10:21:18 +0200

    editing init game file

--- init_game.js
+++ init_game.js
@@ -3,7 +3,9 @@ const prompt = ps();
 function initializeGame() {
     return new Promise(function (resolve, reject) {
         let character = prompt("Enter your name: ");
-        resolve(character);
+        setTimeout(() => {
+            resolve(character);
+        }, 10000);
     }).then(data => {
         console.log(`Hi ${data}, the game initialized successfully`);
     });
